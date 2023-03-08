fb309aa2b38b5d3d98679dceef030a29580b5aad
Sillav <gabrielvinipv62@gmail.com>
2023-03-08 20:42:38 -0300

    ADICIONADA MÁSCA NO CAMPO TELEFONE

--- src/forms/mask.ts
+++ src/forms/mask.ts
@@ -1,6 +1,12 @@
 export default () => {
-  return document
-    .querySelectorAll<HTMLInputElement>('[data-mask]')
-    // Continue a máscara aqui 👇
-    
-}
+  return document.querySelectorAll<HTMLInputElement>('[data-mask="phone"]')
+  .forEach( (el) => {
+    el.oninput = () => {
+      el.value = el.value.replace(/\D/g, '')
+      .replace(/^(\d)/, '($1').replace(/^(\(\d{2})(\d)/, '$1) $2')
+      .replace(/(\d{5})(\d{1,5})/, '$1-$2')
+      .replace(/(\d{5}){\d{1,5}}/, '$1-$2')
+      .replace(/(-\d{4})\d+?$/, '$1')
+    }
+  })
+}  
